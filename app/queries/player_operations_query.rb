f86920cb40d2a61b63739ffddabf8bde31bcaf7b
Mingqian Ye <mye@magicleap.com>
2018-03-27 23:52:35 -0400

    work

--- app/queries/player_operations_query.rb
+++ app/queries/player_operations_query.rb
@@ -1,12 +1,12 @@
 class PlayerOperationsQuery
   # returns:  [{"bought"=>6, "tick_id"=>468}, {"noop"=>6, "tick_id"=>467}, {"noop"=>6, "tick_id"=>469}, {"noop"=>6, "tick_id"=>471}, {"noop"=>6, "tick_id"=>472}, {"noop"=>6, "tick_id"=>473}, {"noop"=>6, "tick_id"=>474}, {"noop"=>6, "tick_id"=>475}, {"noop"=>6, "tick_id"=>476}, {"sold"=>6, "tick_id"=>470}]
   def self.count_op_types_for(tushare_code:, num_points:, ktype:, start_date:)
-    Operation
-      .joins(:game, :tick)
-      .where('games.tushare_code = ?',     tushare_code)
-      .where('games.num_points = ?',       num_points)
-      .where('games.ktype = ?',            ktype)
-      .where('DATE(games.start_date) = ?', start_date)
+    Game
+      .joins(:operations, :ticks)
+      .where(tushare_code: tushare_code)
+      .where(num_points:   num_points)
+      .where(ktype:        ktype)
+      .where(start_date:   start_date)
       .group('operations.op_type, operations.tick_id')
       .order('ticks.date')
       .select('operations.tick_id, operations.op_type, COUNT(*) AS count')
