42ce2b94d96ce6c6f8cd4898310b9c367adbf9a2
Mingqian Ye <mye@magicleap.com>
2018-04-21 02:15:06 -0400

    work

--- app/queries/shadow_query.rb
+++ app/queries/shadow_query.rb
@@ -1,14 +1,17 @@
 class ShadowQuery
   def self.shadow_games_for(tushare_code:, num_points:, ktype:, start_date:, open_id:)
-    Game.joins(:player)
-        .where(tushare_code: tushare_code,
-               num_points:   num_points,
-               ktype:        ktype)
-        .where('DATE(start_date) = ?', start_date)
-        .where('players.open_id <> ?', open_id)
-        .group('players.open_id')
-        .includes(:player)
+    game_ids = Game.joins(:player)
+                   .where(tushare_code: tushare_code,
+                          num_points:   num_points,
+                          ktype:        ktype)
+                   .where('DATE(start_date) = ?', start_date)
+                   .where('players.open_id <> ?', open_id)
+                   .group('players.id')
+                   .pluck('MAX(games.id)')
+
+    Game.includes(:player)
         .includes(:ticks)
         .includes(:operations)
+        .where(id: game_ids)
   end
 end
