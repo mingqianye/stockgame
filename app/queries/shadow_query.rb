beede2129652a8573dddf72090ec85a03c88d582
Mingqian Ye <mye@magicleap.com>
2018-04-21 01:32:14 -0400

    work

--- app/queries/shadow_query.rb
+++ app/queries/shadow_query.rb
@@ -1,14 +1,14 @@
 class ShadowQuery
   def self.shadow_games_for(tushare_code:, num_points:, ktype:, start_date:, open_id:)
     Game.joins(:player)
-        .includes(:players)
-        .includes(:ticks)
-        .includes(:operations)
         .where(tushare_code: tushare_code,
                num_points:   num_points,
                ktype:        ktype)
         .where('DATE(start_date) = ?', start_date)
         .where('players.open_id <> ?', open_id)
         .group('players.open_id')
+        .includes(:player)
+        .includes(:ticks)
+        .includes(:operations)
   end
 end
